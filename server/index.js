aa2e5da41584710679ed66757c66fb7cd4105389
Blake Crawford <blake.crawford@outlook.com>
2022-11-30 00:04:07 -0800

    verified server routes are working as intended with mock response

--- server/index.js
+++ server/index.js
@@ -9,41 +9,42 @@ app.use(express.json());
 
 // get questions
 app.get('/qa/questions/:product_id', (req, res) => {
-  
+  res.send('questions route')
 });
 
 // get answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
-  
+  res.send('answers route')
 });
 
 // post a question
 app.post('/qa/questions', (req, res) => {
-  
+  res.send('post a question')
 });
 
 // post an answer
 app.post('/qa/questions/:question_id/answers', (req, res) => {
-  
+  res.send('post an answer')
 });
 
 // mark question helpful
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  
+  res.send('mark a question')
 });
 
 // mark answer helpful
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  
+  res.send('mark an answer')
 });
 
 // report question
 app.put('/qa/questions/:question_id/report', (req, res) => {
+  res.send('report a question')
 })
 
 // report answer
 app.put('/qa/answers/:answer_id/report', (req, res) => {
-
+  res.send('report an answer')
 });
 
 
